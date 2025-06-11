bb0be9ff09d903a9866d9a02d801f9913cec7465
Varsema <varsema333@gmail.com>
2025-06-11 13:22:48 +0400

    update

--- client/src/component/CardItem/index.js
+++ client/src/component/CardItem/index.js
@@ -4,6 +4,7 @@ import { CustomButton } from '../Common/CustomButton'
 import { Link } from 'react-router-dom'
 import pin from "../../assets/icons/pin.svg"
 import post from "../../assets/icons/post.svg"
+import person from "../../assets/icons/person.svg"
 
 export const CardItem = ({ item, name }) => {
     return (
@@ -12,18 +13,21 @@ export const CardItem = ({ item, name }) => {
                 <img src={item?.cover} alt='ტესტ' />
             </figure>
             {name === 'cars' && <h1 className={styles.card__title}>{item?.title}</h1>}
-            {name === 'teams' && <h1 className={styles.card__title} style={{ backgroundColor: '#CEE1F2' }}>{item?.name}</h1>}
+            {name === 'teams' && <h1 className={styles.card__title} style={{ backgroundColor: '#CEE1F2' }}>
+                <img src={person} alt='person' style={{ marginLeft: -20, position: "absolute", display: "inline" }} />
+                {item?.name}
+            </h1>}
             {name === 'blogs' && <h1 className={styles.card__title}>{item?.title?.ka}</h1>}
             <div className={styles.card__body}>
                 {/* blog description */}
                 {item?.desc && `${item?.desc?.ka}`}
                 {item?.location && <div className={styles.icontext}><img src={pin} alt='pin' /><p>{item?.location}</p></div>}
-                {item?.location && <div className={styles.icontext}><img src={post} alt='pin' /><p>sale@mctrans.ge</p></div>}
+                {item?.location && <div className={styles.icontext}><img src={post} alt='post' /><p>sale@mctrans.ge</p></div>}
                 {item?.location && <CustomButton >
-                    <Link className={styles['btn__link']} style={{ width: 205, height: 5}}>
-                    <div className='d-flex justify-content-center align-items-center'>
-                        <p style={{ position: "fixed"}}>მოგვწერეთ</p>
-                    </div>                       
+                    <Link className={styles['btn__link']} style={{ width: 205, height: 5 }}>
+                        <div className='d-flex justify-content-center align-items-center'>
+                            <p style={{ position: "fixed" }}>მოგვწერეთ</p>
+                        </div>
                     </Link>
                 </CustomButton>}
                 {/* car */}
