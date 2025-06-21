23a7e83a91a433ae9dc9113c3dd9b61855c872a8
Varsema <varsema333@gmail.com>
2025-06-21 14:12:39 +0400

    update

--- client/src/component/MainSlider/Body.js
+++ client/src/component/MainSlider/Body.js
@@ -6,9 +6,22 @@ import hatckback from "../../assets/icons/hatchback.svg"
 import hybrid from "../../assets/icons/hybrid.svg"
 import sedan from "../../assets/icons/sedan.svg"
 import suv from "../../assets/icons/suv.svg"
+import kutxisS from "../../assets/icons/kutxisshavi.svg"
+import kutxisT from "../../assets/icons/kutxis2.svg"
+import { Link } from 'react-router-dom'
 
 
 export default function Body() {
+
+    const data = [
+        { link: "/test", icon: `${suv}`, name: "SUV" },
+        { link: "/test", icon: `${sedan}`, name: "Sedan" },
+        { link: "/test", icon: `${hatckback}`, name: "Hatckback" },
+        { link: "/test", icon: `${coupe}`, name: "Coupe" },
+        { link: "/test", icon: `${hybrid}`, name: "Hybrid" },
+    ]
+
+
     return (
         <div className={styles.body}>
             <p className={styles.tag1}>Find cars for sale and for rent near you</p>
@@ -20,21 +33,27 @@ export default function Body() {
                 <CustomButton>
                     <p className={styles.btn}>
                         View Inventory
+                        <img  src={kutxisT} style={{ position: "absolute", display: "inline-flex", 
+                                marginLeft: 4, width: 11, height: 11, marginTop: 2}}/>
                     </p>
                 </CustomButton>
                 <CustomButton light>
                     <p className={`${styles.btn} ${styles.btn__white}`}>
-                    Contact Us
+                        Contact Us
+                        <img src={kutxisS} style={{ position: "absolute", display: "inline-flex", 
+                                marginLeft: 4, width: 11, height: 11, marginTop: 2}}/>
                     </p>
                 </CustomButton>
             </div>
             <p className={styles.tag2}>Or Browse Featured Model</p>
             <div className={styles.features}>
-                <li><img src={suv}/>SUV</li>
-                <li><img src={sedan}/>Sedan</li>
-                <li><img src={hatckback}/>Hatchback</li>
-                <li><img src={coupe}/>Coupe</li>
-                <li><img src={hybrid}/>Hybrid</li>
+                {data.map((item) => {
+                    return (
+                        <CustomButton light>
+                            <Link to={item.link} className={styles.minibtn} style={{ marginTop: 5}}><img src={item.icon} /><p className={styles.linktxt} style={{ display: "inline-flex", paddingBottom: 5}}>{item.name}</p></Link>
+                        </CustomButton>
+                    )
+                })}
             </div>
         </div>
     )
