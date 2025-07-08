19cd0d826ce19ba065412eaea0904df5a3bd0052
Davit Gelashvili <datoogelashvili@gmail.com>
2025-07-08 19:24:09 +0400

    upload

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5001;
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5001',
-    'https://mctrans.gelashvili.site'
+    'https://mctrans.ge'
 ];
 
 const corsOptions = {
