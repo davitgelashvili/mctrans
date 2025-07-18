f52f707263383ae4c6727bf4541b5cb36b4d1e9c
Davit Gelashvili <datoogelashvili@gmail.com>
2025-07-18 16:38:06 +0400

    fix

--- server/allRouter/VinRouter.js
+++ server/allRouter/VinRouter.js
@@ -5,6 +5,10 @@ require("dotenv").config();
 vinRouter.post("/", async (req, res) => {
   try {
     const { vin, container } = req.body;
+    
+    console.log(process.env.AUTH_KEY)
+
+    console.log("მიღებული მონაცემები:", { vin, container });
 
     if (!vin && !container) {
       return res.status(400).json({ error: "VIN ან კონტეინერი საჭიროა." });
@@ -14,22 +18,24 @@ vinRouter.post("/", async (req, res) => {
     if (vin) formData.append("vincode", vin);
     if (!vin && container) formData.append("container", container);
 
-    const response = await fetch("https://db.mctrans.ge/website", {
+    const fetchRes = await fetch("https://db.mctrans.ge/website", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        "X-Auth-Key": process.env.AUTH_KEY // ან ხელით ჩაწერე
+        "X-Auth-Key": process.env.AUTH_KEY
       },
       body: formData.toString()
     });
 
-    const text = await response.text();
+    const text = await fetchRes.text();
     const cleanText = text.replace(/^\uFEFF/, ''); // BOM strip
-    const data = JSON.parse(cleanText);
 
+    console.log("API პასუხი:", cleanText);
+
+    const data = JSON.parse(cleanText);
     res.json(data);
   } catch (err) {
-    console.error(err);
+    console.error("შეცდომა vincheck-ში:", err);
     res.status(500).json({ error: "სერვერის შეცდომა." });
   }
 });
