1266de60572187c1d4c26c939f82e4dcc63278e2
Davit Gelashvili <datoogelashvili@gmail.com>
2025-07-22 11:16:32 +0400

    fixed

--- server/server.js
+++ server/server.js
@@ -1,19 +1,18 @@
 const express = require('express');
-const mainrRouter = require('./router.js')
+const mainrRouter = require('./router.js');
 const mongoose = require("mongoose");
 const path = require('path');
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const os = require('os')
+const os = require('os');
 
 const PORT = process.env.PORT || 5001;
 
-// დამატებული პროდუქციის დომენი
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:5001',
-    '198.177.120.132'
+    'http://198.177.120.132', // თუ ეს უნდა იყოს დომენი, http://-ით დაამატე
 ];
 
 const corsOptions = {
@@ -27,41 +26,41 @@ const corsOptions = {
     credentials: true
 };
 
+// --- MIDDLEWARE ---
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// --- ROUTES ---
 app.get('/api/ip', (req, res) => {
-    const interfaces = os.networkInterfaces()
-    let serverIp = 'unknown'
-
+    const interfaces = os.networkInterfaces();
+    let serverIp = 'unknown';
     for (let iface of Object.values(interfaces)) {
         for (let config of iface) {
             if (config.family === 'IPv4' && !config.internal) {
-                serverIp = config.address
-                break
+                serverIp = config.address;
+                break;
             }
         }
-        if (serverIp !== 'unknown') break
+        if (serverIp !== 'unknown') break;
     }
-
-    res.json({ ip: serverIp })
-})
+    res.json({ ip: serverIp });
+});
 
 app.use("/api", mainrRouter);
 
-// React-ის ბილდის ფაილების სერვინგი
+// React build serving
 app.use(express.static(path.join(__dirname, '../client/build')));
-
-// ყველა სხვა route -> index.html (SPA-ის მხარდაჭერა)
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
+// --- SERVER START ---
 app.listen(PORT, () => {
     console.log(`სერვერი აჭრილია პორტზე ${PORT}`);
 });
 
+// --- DATABASE CONNECTION ---
 mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => {
